b62e0def2a10f5f196f7400ceb71238ce9863976
hitakshi-25 <210830116001@dsuni.ac.in>
2025-04-23 16:07:31 +0530

    😊 Facial Detection: OpenCV emotion detection enabled

--- app/services/facial_detector.py
+++ app/services/facial_detector.py
@@ -11,4 +11,3 @@ def detect_emotion_from_image(image_path: str):
     analysis = DeepFace.analyze(img_path=image_path, actions=["emotion"], enforce_detection=False)
     emotion = analysis[0]['dominant_emotion']
     return emotion
-
