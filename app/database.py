8eae775eb6c69c59f4a5774627379a8735da2ae9
hitakshi-25 <210830116001@dsuni.ac.in>
2025-04-23 15:43:32 +0530

    🛢️ PostgreSQL database connection handler added

--- app/database.py
+++ app/database.py
@@ -1,16 +1,17 @@
-# SQLAlchemy setup
 import os
+import psycopg2
+from psycopg2.extras import RealDictCursor
 from dotenv import load_dotenv
-import mysql.connector
 
 load_dotenv()
 
 def get_db():
-    return mysql.connector.connect(
+    return psycopg2.connect(
         host=os.getenv("DB_HOST"),
+        dbname=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASS"),
-        database=os.getenv("DB_NAME")
+        cursor_factory=RealDictCursor
     )
 
 def save_notification(user_id, message):
